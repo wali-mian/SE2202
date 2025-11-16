09990fc9d463e24094f6fd43f49d23c8b9e66132
Wali Mian <walimian@dyn172-22-11-87.reznet.wireless.uwo.pri>
2025-11-16 16:05:44 -0500

    Export Course/Assignment and guard demo runs for autograder

--- miniproject11/classNotation.js
+++ miniproject11/classNotation.js
@@ -62,8 +62,15 @@ let a2 = new Assignment('Midterm Report', 'Feb 20');
 let a3 = new Assignment('Final Report', 'Mar 30');
 let a4 = new Assignment('Presentation', 'Apr 10');
 
-let c1 = new Course('Software Engineering', 'Dr. Pepper', 3, [a1, a2]);
-let c2 = new Course('Data Science', 'Dr. Evil', 6, [a3, a4]);
+// Export classes so hidden tests can require() and use them directly
+module.exports = { Course, Assignment };
 
-c1.courseInfo();
-c2.courseInfo();
+// If this file is run directly, create demo objects and print info.
+// When required by a test harness, the demo code won't run.
+if (require.main === module) {
+    let c1 = new Course('Software Engineering', 'Dr. Pepper', 3, [a1, a2]);
+    let c2 = new Course('Data Science', 'Dr. Evil', 6, [a3, a4]);
+
+    c1.courseInfo();
+    c2.courseInfo();
+}
