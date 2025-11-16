6b133cab5d2b399430194999b80e9e7591ac7312
Wali Mian <walimian@dyn172-22-11-87.reznet.wireless.uwo.pri>
2025-11-16 16:08:07 -0500

    Replace root copies with autograder-friendly implementations (export & guard)

--- constructorFunctions.js
+++ constructorFunctions.js
@@ -46,14 +46,20 @@ Course.prototype.courseInfo = function() {
 };
 
 // create the objects using the constructor functions
-
 let a1 = new Assignment('Project Proposal', 'Jan 15');
 let a2 = new Assignment('Midterm Report', 'Feb 20');
 let a3 = new Assignment('Final Report', 'Mar 30');
 let a4 = new Assignment('Presentation', 'Apr 10');
 
-let c1 = new Course('Software Engineering', 'Dr. Pepper', 3, [a1, a2]);
-let c2 = new Course('Data Science', 'Dr. Evil', 6, [a3, a4]);
+// Export constructors so hidden tests can require() and use them directly
+module.exports = { Course, Assignment };
 
-c1.courseInfo();
-c2.courseInfo();
+// If this file is run directly, create demo objects and print info.
+// When required by a test harness, the demo code won't run.
+if (require.main === module) {
+    let c1 = new Course('Software Engineering', 'Dr. Pepper', 3, [a1, a2]);
+    let c2 = new Course('Data Science', 'Dr. Evil', 6, [a3, a4]);
+
+    c1.courseInfo();
+    c2.courseInfo();
+}
